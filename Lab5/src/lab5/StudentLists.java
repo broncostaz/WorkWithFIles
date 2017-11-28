37a781e77e81bfea734bf4fac8c77e5e52f6392d
readoc <readoc@users.noreply.github.com>
2017-11-28 16:17:27 -0500

    Initialized Repository
    Files committed to repository.
    
    Remember to remove placeholder text in the two (2) .txt files.

---
+++ Lab5/src/lab5/StudentLists.java
@@ -0,0 +1,54 @@
+/*************************************
+* Programmer: Your Name
+* classID: Your classID
+* Lab5: WMU GPA-O-Tron
+* CIS 2600: Business Application Programming
+* Fall 2017
+* Due date: XX/XX/XX
+* Date completed: XX/XX/XX
+**************************************
+* Program Explanation
+* 
+* Write about what your program does, how it does it, and why.
+* 
+* Make sure to explain how this Java file works in the lab as
+* well as how it interacts with the other two Java object files.
+* 
+**************************************/
+
+/*
+Document your code.
+
+Discussing how it accomplishes what it does 
+is as important as noting what it does.
+
+Make sure to explain the entire file interaction process in detail.
+
+##Remember to use these paths:##
+
+Paths.get("StudentsGoodStanding.txt");
+Paths.get("StudentsAcademicProbation.txt");
+
+There are text files in your NetBean folder.
+
+*/ 
+
+//=============================Watch the line==================================>
+
+package lab5;
+
+/**
+ *
+ * @author GitHub ID
+ * 
+ */
+
+public class StudentLists {
+
+
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
